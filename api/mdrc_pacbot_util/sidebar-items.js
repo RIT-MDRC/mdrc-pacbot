d4b20b102fbfb1f71ea9e9f1c23029262dbd310f
MJE10 <MJE10@users.noreply.github.com>
2023-09-20 01:18:27 +0000

    deploy: 166856cb8853cb5508b82fc12c62a637c84687f1

--- api/mdrc_pacbot_util/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["grid","gui","standard_grids"]};
--- api/source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"mdrc_pacbot_util":["",[["gui",[],["mod.rs","transforms.rs"]]],["grid.rs","lib.rs","standard_grids.rs"]]\
-}');
-createSourceSidebar();
