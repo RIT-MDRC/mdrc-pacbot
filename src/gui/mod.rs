166856cb8853cb5508b82fc12c62a637c84687f1
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-09-19 21:18:15 -0400

    Fix overlapping walls & off-by-1 error causing space around the edge (#3)
    * Fix overlapping walls & off-by-1 error causing space around the edge
    
    * Fix grid painter going outside its boundaries
    
    * Remove margin from grid painter
    
    * Default grid pacman
    
    * Add ability to change the grid with a dropdown and display pointer coordinates in the corner

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,8 +2,9 @@
 
 pub mod transforms;
 
-use egui::{Color32, Pos2, Rect, Rounding, Stroke, Ui};
+use egui::{Color32, Frame, Pos2, Rect, Rounding, Stroke, Ui};
 
+use crate::standard_grids::StandardGrid;
 use crate::{grid::ComputedGrid, standard_grids};
 
 use self::transforms::Transform;
@@ -20,30 +21,43 @@ pub fn run_gui() {
 }
 
 struct App {
+    selected_grid: StandardGrid,
     grid: ComputedGrid,
+    pointer_pos: String,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
+            selected_grid: StandardGrid::Pacman,
             grid: ComputedGrid::try_from(standard_grids::GRID_PACMAN).unwrap(),
+            pointer_pos: "".to_string(),
         }
     }
 }
 
 impl App {
-    fn draw_game(&mut self, ui: &mut Ui) {
-        let (canvas_width, canvas_height) = (ui.available_width(), ui.available_height());
+    fn draw_game(&mut self, ctx: &egui::Context, ui: &mut Ui) {
+        let rect = ui.max_rect();
+
         let world_to_screen = Transform::new_letterboxed(
             Pos2::new(-1.0, 32.0),
             Pos2::new(32.0, -1.0),
-            Pos2::new(0.0, 0.0),
-            Pos2::new(canvas_width, canvas_height),
+            Pos2::new(rect.left(), rect.top()),
+            Pos2::new(rect.right(), rect.bottom()),
         );
+
+        self.pointer_pos = match ctx.pointer_latest_pos() {
+            None => "".to_string(),
+            Some(pos) => {
+                let pos = world_to_screen.inverse().map_point(pos);
+                format!("({:.1}, {:.1})", pos.x, pos.y)
+            }
+        };
 
         let wall_color = Color32::LIGHT_GRAY;
 
-        let painter = ui.painter();
+        let painter = ui.painter_at(rect);
         for wall in self.grid.walls() {
             let (p1, p2) = world_to_screen.map_wall(wall);
             painter.rect(
@@ -60,12 +74,39 @@ impl App {
             Color32::BLUE,
         )
     }
+
+    fn add_grid_variants(&mut self, ui: &mut Ui) {
+        egui::ComboBox::from_label("")
+            .selected_text(format!("{:?}", self.selected_grid))
+            .show_ui(ui, |ui| {
+                StandardGrid::get_all().iter().for_each(|grid| {
+                    if ui
+                        .selectable_value(&mut self.selected_grid, *grid, format!("{:?}", grid))
+                        .clicked()
+                    {
+                        self.grid = ComputedGrid::try_from(grid.get_grid()).unwrap();
+                    }
+                });
+            });
+    }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            self.draw_game(ui);
+        egui::TopBottomPanel::top("menu").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+                    self.add_grid_variants(ui);
+                });
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                    ui.label(&self.pointer_pos);
+                });
+            });
         });
+        egui::CentralPanel::default()
+            .frame(Frame::none().fill(ctx.style().visuals.panel_fill))
+            .show(ctx, |ui| {
+                self.draw_game(ctx, ui);
+            });
     }
 }
