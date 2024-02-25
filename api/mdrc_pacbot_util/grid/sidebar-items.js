db64964a11baf0eb4fd358fb1e55b52cdb903108
qxzcode <qxzcode@users.noreply.github.com>
2024-02-25 00:26:22 +0000

    deploy: d7167dcf2c5a19f8a87ee8fda3d119d71cfdc85e

---
+++ api/mdrc_pacbot_util/grid/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["GRID_COLS","GRID_ROWS"],"enum":["Direction"],"fn":["facing_direction","validate_grid"],"mod":["standard_grids"],"struct":["ComputedGrid","IntLocation","Wall"],"type":["Grid"]};
---
+++ api/mdrc_pacbot_util/robot/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["DistanceSensor","IMU","Motor","OmniWheel","Robot"]};
