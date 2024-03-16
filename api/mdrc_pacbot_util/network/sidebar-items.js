27c9cc73bd8fd0e89e73ef9de765108ce24115d8
MJE10 <MJE10@users.noreply.github.com>
2024-03-16 19:09:43 +0000

    deploy: f7b33a3a187e97864905ac1113834503d5a3112b

---
+++ api/mdrc_pacbot_util/network/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["GSConnState"],"fn":["poll_gs","reconnect_pico","recv_pico","send_motor_commands"],"struct":["GameServerConn","NetworkPluginData","PacbotCommand","PacbotSensors","PacbotSensorsRecvTime","PicoConnection"]};
