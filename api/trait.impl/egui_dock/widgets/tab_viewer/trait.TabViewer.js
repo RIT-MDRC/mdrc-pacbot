869e42815ec5793c2dc85361f48f2c22ea9358df
MJE10 <MJE10@users.noreply.github.com>
2024-03-16 19:33:15 +0000

    deploy: 0aa8a9821c2fbb9367b621fee0eb293869fd5cd8

---
+++ api/trait.impl/egui_dock/widgets/tab_viewer/trait.TabViewer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mdrc_pacbot":[["impl&lt;'a&gt; TabViewer for <a class=\"struct\" href=\"mdrc_pacbot/gui/struct.TabViewer.html\" title=\"struct mdrc_pacbot::gui::TabViewer\">TabViewer</a>&lt;'a&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
