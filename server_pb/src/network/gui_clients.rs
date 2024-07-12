de3cb38c6c2196a35a6eb4b18c772cf82f3fbac3
MJE10 <63426573+MJE10@users.noreply.github.com>
2024-07-12 16:59:32 -0400

    simplify server_pb websocket listener

--- server_pb/src/network/gui_clients.rs
+++ server_pb/src/network/gui_clients.rs
@@ -1,141 +1,85 @@
-use crate::{status, App};
+use std::collections::HashMap;
+use std::pin::pin;
+use std::sync::{Arc, Mutex};
+
+use futures_channel::mpsc::{UnboundedReceiver, UnboundedSender};
+use futures_util::{select, FutureExt, StreamExt};
+use simple_websockets::{Event, Message, Responder};
+
 use core_pb::constants::GUI_LISTENER_PORT;
 use core_pb::messages::server_status::ServerStatus;
 use core_pb::messages::GuiToGameServerMessage;
 use core_pb::{bin_decode, bin_encode};
-use futures_channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-use futures_util::future::{select, Either};
-use futures_util::{future, pin_mut, StreamExt, TryStreamExt};
-use std::collections::HashMap;
-use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::time::sleep;
-use tokio_tungstenite::tungstenite::Message;
 
-type Tx = UnboundedSender<Message>;
-type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+use crate::{status, App};
 
 pub async fn listen_for_gui_clients(
     app: Arc<Mutex<App>>,
     incoming: UnboundedSender<GuiToGameServerMessage>,
     mut outgoing: UnboundedReceiver<ServerStatus>,
 ) -> ! {
-    let state = PeerMap::new(Mutex::new(HashMap::new()));
-    let addr = format!("0.0.0.0:{GUI_LISTENER_PORT}");
+    let event_hub = simple_websockets::launch(GUI_LISTENER_PORT).unwrap();
+    println!("Listening on 0.0.0.0:{GUI_LISTENER_PORT}");
+
+    let mut gui_clients = HashMap::new();
 
     loop {
-        *state.lock().unwrap() = HashMap::new();
+        let event_fut = pin!(event_hub.poll_async());
+        let outgoing_fut = pin!(outgoing.next());
 
-        match TcpListener::bind(addr.clone()).await {
-            Ok(listener) => {
-                println!("Listening on: {addr}");
-
-                loop {
-                    let acc = listener.accept();
-                    pin_mut!(acc);
-
-                    match select(acc, outgoing.next()).await {
-                        Either::Left((Ok((stream, addr)), _)) => {
-                            tokio::spawn(handle_gui_client(
-                                app.clone(),
-                                state.clone(),
-                                stream,
-                                addr,
-                                incoming.clone(),
-                            ));
-                        }
-                        Either::Left((Err(e), _)) => {
-                            eprintln!("Failed to accept gui client: {e:?}");
-                            println!("Waiting 5 seconds for gui listener restart...");
-                            sleep(Duration::from_secs(5)).await;
-                            break;
-                        }
-                        Either::Right((Some(msg), _)) => {
-                            let bytes = Message::Binary(bin_encode(msg).unwrap());
-                            for (addr, c) in state.lock().unwrap().iter_mut() {
-                                match c.unbounded_send(bytes.clone()) {
-                                    Ok(()) => {}
-                                    Err(e) => {
-                                        eprintln!(
-                                            "Failed to send message to gui client {addr}: {e:?}"
-                                        );
-                                    }
-                                }
-                            }
-                        }
-                        Either::Right((None, _)) => {
-                            panic!("Outgoing network messages channel closed");
-                        }
+        select! {
+            event = event_fut.fuse() => {
+                handle_event(&app, &incoming, &mut gui_clients, event);
+            }
+            outgoing_msg = outgoing_fut.fuse() => {
+                let msg = Message::Binary(bin_encode(outgoing_msg.unwrap()).unwrap());
+                for (id, responder) in &mut gui_clients {
+                    if !responder.send(msg.clone()) {
+                        eprintln!("Error sending status to client #{id}, already closed")
                     }
                 }
-            }
-            Err(e) => {
-                eprintln!("Failed to bind: {e:?}");
-                println!("Waiting 5 seconds for gui listener restart...");
-                sleep(Duration::from_secs(5)).await;
             }
         }
     }
 }
 
-async fn handle_gui_client(
-    app: Arc<Mutex<App>>,
-    peer_map: PeerMap,
-    raw_stream: TcpStream,
-    addr: SocketAddr,
-    incoming_msg: UnboundedSender<GuiToGameServerMessage>,
+fn handle_event(
+    app: &Arc<Mutex<App>>,
+    incoming: &UnboundedSender<GuiToGameServerMessage>,
+    gui_clients: &mut HashMap<u64, Responder>,
+    event: Event,
 ) {
-    println!("Incoming TCP connection from gui client: {}", addr);
-
-    let ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
-        Ok(x) => x,
-        Err(e) => {
-            eprintln!("Error during the gui client websocket handshake occurred: {e:?}");
-            return;
+    match event {
+        Event::Connect(id, responder) => {
+            println!("Gui client #{id} connected");
+            status(app, |s| {
+                s.gui_clients += 1;
+                println!("{} gui client(s) are connected", s.gui_clients);
+            });
+            gui_clients.insert(id, responder);
         }
-    };
-    println!("WebSocket connection from gui client established: {}", addr);
-
-    // Insert the write part of this peer to the peer map.
-    let (tx, rx) = unbounded();
-    peer_map.lock().unwrap().insert(addr, tx);
-    status(&app, |s| {
-        s.gui_clients += 1;
-        println!("{} client(s) are connected", s.gui_clients);
-    });
-
-    let (outgoing, incoming) = ws_stream.split();
-
-    // Future to loop through incoming messages
-    let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!("Received a message from gui client {}", addr);
-        match msg {
-            Message::Binary(bytes) => match bin_decode(&bytes) {
-                Ok(msg) => incoming_msg.unbounded_send(msg).unwrap(),
-                Err(e) => eprintln!("Error decoding message from {addr}: {e:?}"),
-            },
-            Message::Close(_) => {
-                println!("gui client {addr} is closing the connection");
+        Event::Disconnect(id) => {
+            println!("Gui client #{id} disconnected");
+            status(app, |s| {
+                s.gui_clients -= 1;
+                println!("{} gui client(s) remaining", s.gui_clients);
+            });
+            gui_clients.remove(&id);
+        }
+        Event::Message(id, msg) => {
+            println!("Received a message from gui client {}", id);
+            match msg {
+                Message::Binary(bytes) => match bin_decode(&bytes) {
+                    Ok(msg) => incoming.unbounded_send(msg).unwrap(),
+                    Err(e) => eprintln!(
+                        "Error decoding message from {id}: {e:?}, {} bytes",
+                        bytes.len()
+                    ),
+                },
+                Message::Text(text) => {
+                    eprintln!("Received strange message from gui client {id}: {text}")
+                }
             }
-            m => eprintln!("Received strange message from gui client {addr}: {m:?}"),
         }
-
-        future::ok(())
-    });
-
-    // Future to send queued messages to socket
-    let receive_from_others = rx.map(Ok).forward(outgoing);
-
-    // Do both
-    pin_mut!(broadcast_incoming, receive_from_others);
-    future::select(broadcast_incoming, receive_from_others).await;
-
-    println!("gui client {} disconnected", &addr);
-    peer_map.lock().unwrap().remove(&addr);
-    status(&app, |s| {
-        s.gui_clients -= 1;
-        println!("{} client(s) remaining", s.gui_clients);
-    });
+    }
 }
