8d7ee846c4ad5580f289862f5231ddc170629625
MJE10 <MJE10@users.noreply.github.com>
2024-08-13 16:13:05 +0000

    deploy: ce5d78d1057fcd8b6e7681254a3810538c93d476

--- api/core_pb/messages/settings/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["KnownRLModel","StrategyChoice"],"struct":["ConnectionSettings","DriveSettings","GameServerSettings","PacbotSettings","RobotSettings","SimulationSettings"]};
