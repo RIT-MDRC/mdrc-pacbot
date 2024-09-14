4d0aedab6c78b0124dc098d3abb329c2c5b03c62
MJE10 <MJE10@users.noreply.github.com>
2024-09-14 17:30:10 +0000

    deploy: 4e491b665450c7775106533388fb90f533fc8751

---
+++ api/core_pb/messages/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["GameServerCommand","GuiToServerMessage","NetworkStatus","RobotToServerMessage","ServerToGuiMessage","ServerToRobotMessage","ServerToSimulationMessage"],"mod":["ota","server_status","settings"],"struct":["FrequentServerToRobot","MotorControlStatus","RobotStatus","SensorData","SimulationToServerMessage"]};
---
+++ api/trait.impl/embedded_io/trait.ErrorType.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sim_pb":[["impl ErrorType for <a class=\"struct\" href=\"sim_pb/driving/network/struct.TcpStreamReadWrite.html\" title=\"struct sim_pb::driving::network::TcpStreamReadWrite\">TcpStreamReadWrite</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
