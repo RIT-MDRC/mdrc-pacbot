d8c151f502d77130f35ed4731c6c49d4757273c0
MJE10 <MJE10@users.noreply.github.com>
2024-09-21 01:10:09 +0000

    deploy: fe8053978b01b6a4d4ddcd1d6588117a9fec29d8

---
+++ api/trait.impl/eframe/epi/trait.App.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gui_pb":[["impl App for <a class=\"struct\" href=\"gui_pb/struct.App.html\" title=\"struct gui_pb::App\">App</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
