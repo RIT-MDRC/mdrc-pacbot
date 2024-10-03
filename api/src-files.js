b2e178d04f3c32d6c25358c8c8df24fde2332c03
MJE10 <MJE10@users.noreply.github.com>
2024-10-03 04:18:36 +0000

    deploy: b49393fc6f602155c4235e14509eb426b28ab865

---
+++ api/src-files.js
@@ -0,0 +1,7 @@
+var srcIndex = new Map(JSON.parse('[\
+["core_pb",["",[["driving",[],["mod.rs","motors.rs","network.rs","peripherals.rs"]],["grid",[],["computed_grid.rs","mod.rs","standard_grid.rs"]],["messages",[],["mod.rs","ota.rs","server_status.rs","settings.rs"]],["util",[],["average_rate.rs","mod.rs","moving_average.rs","stopwatch.rs","utilization.rs"]]],["constants.rs","drive_system.rs","lib.rs","localization.rs","names.rs","robot_definition.rs","threaded_websocket.rs"]]],\
+["gui_pb",["",[["drawing",[],["game.rs","mod.rs","motors.rs","over_the_air.rs","replay_manager.rs","settings.rs","tab.rs","timings.rs","widgets.rs"]]],["colors.rs","input.rs","main.rs","replay.rs","transform.rs"]]],\
+["server_pb",["",[],["high_level.rs","main.rs","network.rs","ota.rs","sockets.rs"]]],\
+["sim_pb",["",[["driving",[],["mod.rs","motors.rs","network.rs","peripherals.rs"]]],["delayed_value.rs","main.rs","network.rs","physics.rs"]]]\
+]'));
+createSrcSidebar();
