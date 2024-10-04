6356196de9e87e30dd43705d9f3d5327cc04044b
MJE10 <MJE10@users.noreply.github.com>
2024-10-04 16:24:00 +0000

    deploy: 1eba505d33253a228884f006a6c56fcd853a734e

---
+++ api/trait.impl/core_pb/driving/peripherals/trait.RobotPeripheralsBehavior.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sim_pb":[["impl <a class=\"trait\" href=\"core_pb/driving/peripherals/trait.RobotPeripheralsBehavior.html\" title=\"trait core_pb::driving::peripherals::RobotPeripheralsBehavior\">RobotPeripheralsBehavior</a> for <a class=\"struct\" href=\"sim_pb/driving/peripherals/struct.SimPeripherals.html\" title=\"struct sim_pb::driving::peripherals::SimPeripherals\">SimPeripherals</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
