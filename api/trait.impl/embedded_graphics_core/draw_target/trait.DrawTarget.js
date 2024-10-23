0bc409bceb7515df218643381af50c48bed8b868
Ben-RIT <Ben-RIT@users.noreply.github.com>
2024-10-23 02:35:38 +0000

    deploy: 5e5355c44efd8165a31713f03ec2a2254b0a1503

--- api/trait.impl/embedded_graphics_core/draw_target/trait.DrawTarget.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"sim_pb":[["impl DrawTarget for <a class=\"struct\" href=\"sim_pb/driving/peripherals/struct.SimDisplay.html\" title=\"struct sim_pb::driving::peripherals::SimDisplay\">SimDisplay</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
