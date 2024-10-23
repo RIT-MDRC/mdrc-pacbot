8ae4ca8b027b553b0ca5739ce8741e6f2b26aa63
Ben-RIT <Ben-RIT@users.noreply.github.com>
2024-10-23 02:37:22 +0000

    deploy: 5e5355c44efd8165a31713f03ec2a2254b0a1503

---
+++ api/core_pb/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["bin_decode","bin_encode"],"macro":["console_error","console_log"],"mod":["constants","drive_system","driving","grid","localization","messages","names","pure_pursuit","robot_definition","threaded_websocket","util"]};
