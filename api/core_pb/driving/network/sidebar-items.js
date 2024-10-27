7a9d5f5e69d3b9260d3cafa609b7e36af46407e9
MJE10 <MJE10@users.noreply.github.com>
2024-10-27 00:58:26 +0000

    deploy: 55b026b08c8abc5b59efaceac462bc6ecf579607

--- api/core_pb/driving/network/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["DEFAULT_NETWORK"],"fn":["network_task","next_event","read_rest","write"],"struct":["NetworkScanInfo"],"trait":["RobotNetworkBehavior"]};
--- api/core_pb/names/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["NUM_ROBOT_NAMES"],"enum":["RobotName"]};
