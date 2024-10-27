e6adf42b0c6b34a954ab910a45a8f5d1e28905f0
MJE10 <MJE10@users.noreply.github.com>
2024-10-27 00:56:24 +0000

    deploy: 7e29d0485686aaed55848b6b84586dbf0ed240ce

---
+++ api/core_pb/driving/motors/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["motors_task"],"struct":["MotorsData"],"trait":["RobotMotorsBehavior"]};
---
+++ api/core_pb/grid/computed_grid/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["facing_direction","validate_grid"],"struct":["ComputedGrid","Wall"]};
