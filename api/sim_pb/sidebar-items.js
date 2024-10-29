ebffb7ee59b8d168ab5cc8559cde9b3d28a5b5e6
MJE10 <MJE10@users.noreply.github.com>
2024-10-29 23:20:29 +0000

    deploy: f2474980942277e1ad142bd2fd10fe1b579b8397

---
+++ api/sim_pb/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["keyboard_input","main","robot_position_to_game_state","setup_graphics","setup_physics"],"mod":["delayed_value","driving","network","physics"],"struct":["MyApp","RobotReference","Wall"]};
