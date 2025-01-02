581e6d4102868ca321bc72eb494fa628143b0566
MJE10 <MJE10@users.noreply.github.com>
2025-01-02 19:24:15 +0000

    deploy: 85e2c6a182c1c71c249b564cda5eaaf1d839a6c1

--- api/core_pb/localization/sidebar-items.js
+++ api/core_pb/localization/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["CV_ERROR","VECTORS"],"fn":["estimate_location","get_dist","get_estimated_poses","get_sim_ray_cast","wall_at"]};
+window.SIDEBAR_ITEMS = {"constant":["VECTORS"],"fn":["estimate_location","get_dist","get_estimated_poses","get_sim_ray_cast","wall_at"]};
