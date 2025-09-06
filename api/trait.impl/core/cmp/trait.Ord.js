db17b9dc02b7adc2d36367e4f2efc8348b35ffd1
hieyou1 <hieyou1@users.noreply.github.com>
2025-09-06 19:25:29 +0000

    deploy: e90d64a1ff3e216b5ac8b7aad049e3fb00de12ae

---
+++ api/trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["core_pb",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"core_pb/grid/standard_grid/enum.StandardGrid.html\" title=\"enum core_pb::grid::standard_grid::StandardGrid\">StandardGrid</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"core_pb/messages/settings/enum.KnownRLModel.html\" title=\"enum core_pb::messages::settings::KnownRLModel\">KnownRLModel</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"core_pb/names/enum.RobotName.html\" title=\"enum core_pb::names::RobotName\">RobotName</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[842]}
