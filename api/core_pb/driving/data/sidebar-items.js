e1bce4304917d9da442306a6e40055df1c13ebb3
hieyou1 <hieyou1@users.noreply.github.com>
2025-09-27 20:04:08 +0000

    deploy: 6209123d2e709ecac537c50790d538720a498aca

---
+++ api/core_pb/driving/data/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["make_extra_atomic_types"],"struct":["SharedRobotData"]};
---
+++ api/trait.impl/embedded_io/trait.ErrorType.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sim_pb",[["impl ErrorType for <a class=\"struct\" href=\"sim_pb/driving/network/struct.TcpStreamReadWrite.html\" title=\"struct sim_pb::driving::network::TcpStreamReadWrite\">TcpStreamReadWrite</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[203]}
