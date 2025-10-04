2211f784ae2009159d7e98b209aad0f07fa41acc
Ming Creekmore <mcreekmore480@gmail.com>
2025-10-04 15:46:37 -0400

    fix some syntax

--- core_pb/src/util/motion_profile.rs
+++ core_pb/src/util/motion_profile.rs
@@ -1,15 +1,16 @@
 use std::cmp;
 
 struct MotionProfile {
-    a: f32
-    max_v: f32
-    start_v: f32
+    a: f32,
+    max_v: f32,
+    start_v: f32,
     end_v: f32
 }
 
 impl MotionProfile {
     // only calculating the first part
     
+    // gets next velocity to reach (step function)
     fn get_next_v(&mut self, time: f32) -> f32 {
 
         // calculating velocity reached after time
