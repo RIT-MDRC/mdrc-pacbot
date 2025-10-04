3596fb054f42d37ed3717125deeadeb6423ac380
Ming Creekmore <mcreekmore480@gmail.com>
2025-10-04 15:54:40 -0400

    updated comments and variable names

--- core_pb/src/util/motion_profile.rs
+++ core_pb/src/util/motion_profile.rs
@@ -3,22 +3,28 @@ use std::cmp;
 struct MotionProfile {
     a: f32,
     max_v: f32,
-    start_v: f32,
+    current_v: f32,
     end_v: f32
 }
 
 impl MotionProfile {
     // only calculating the first part
     
-    // gets next velocity to reach (step function)
+    /**
+     * Gets the next velocity in the motion profile given the time elapsed.
+     * # Arguments
+     * * `time` - The time to move in seconds
+     */
     fn get_next_v(&mut self, time: f32) -> f32 {
 
         // calculating velocity reached after time
-        let vt = (self.a*time) + self.start_v;
+        let vt = (self.a*time) + self.current_v;
         let v_effective_max = cmp::min(self.max_v, self.end_v);
 
         // next velocity is min
-        cmp::min(vt, v_effective_max)
+        let new_v = cmp::min(vt, v_effective_max);
+        self.current_v = new_v;
+        new_v
     }
 }
 
